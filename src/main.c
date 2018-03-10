fc1ad63e6836242fd6c3e5d2fcf3faeb82f63441
Anna KORUNSKA <akorunsk@e1r7p18.unit.ua>
2018-03-10 12:55:04 +0200

    fixed leaks in gnl

--- src/main.c
+++ src/main.c
@@ -10,25 +10,42 @@
 /*                                                                            */
 /* ************************************************************************** */
 
-#include <stdlib.h>//
+#include <stdlib.h> //
+#include <stdio.h>  //
 #include "lem_in.h"
+#include <fcntl.h>
+
+// int		main(void)
+// {
+// 	int				ant_q;
+// 	t_matrix		m;
+// 	t_array_list	rooms;
+// 	t_path			**p;
+
+// 	ant_q = parse_input(&m, &rooms);
+// 	if (!ant_q)
+// 		return (1);
+// 	p = fill_path_arr(get_shortest_paths(&m), m.end_room);
+// 	farm_manager(p, &rooms, ant_q);
+// 	matrix_free(&m);
+// 	al_clear(&rooms);
+// 	free_path(p);
+	
+// 	//system("leaks lem-in");
+// 	return (0);
+// }
 
 int		main(void)
 {
-	int				ant_q;
-	t_matrix		m;
-	t_array_list	rooms;
-	t_path			**p;
+	char	*buf;
+	int		fd = open("1.txt", O_RDONLY);
 
-	ant_q = parse_input(&m, &rooms);
-	if (!ant_q)
-		return (1);
-	p = fill_path_arr(get_shortest_paths(&m), m.end_room);
-	farm_manager(p, &rooms, ant_q);
-	matrix_free(&m);
-	al_clear(&rooms);
-	free_path(p);
-	
-	//system("leaks lem-in");
+	while (get_next_line(fd, &buf) > 0)
+	{
+		printf("%s\n", buf);
+		free(buf);
+	}
+	free(buf);
+	system("leaks lem-in");
 	return (0);
 }
